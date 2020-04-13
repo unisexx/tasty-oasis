f1a4355e87cf8eeda9d468047eb39bf381ec7aec
unisexx <unisexx@gmail.com>
2020-04-13 22:03:19 +0700

    ติดตั้งครั้งแรก

---
+++ app/Contact.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Spatial;
+
+class Contact extends Model
+{
+    use Spatial;
+    //
+}
---
+++ app/ProductCategory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    // public function parentId()
+    // {
+    //     return $this->belongsTo(self::class);
+    // }
+}
